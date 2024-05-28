1d4c188fb2744930f24ff4ceee95137c4372d8d7
Manish Maharjan <maharjanm96@gmail.com>
2024-05-28 19:12:36 +0545

    Todo Complete

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
-const { addItem, updateItem, deleteItem } = require("../controllers/todoCrud");
+const {
+  addItem,
+  updateItem,
+  deleteItem,
+  getAllItems,
+} = require("../controllers/todoCrud");
 
+router.get("/todo", getAllItems);
 router.post("/todo", addItem);
 router.patch("/todo/:todoId", updateItem);
 router.delete("/todo/:todoId", deleteItem);
